a9540627a1db00899895dc189642f9f868556b26
marlio <jitao.liu82@gmail.com>
2020-05-30 16:07:51 +0800

    add zuul server

---
+++ zuul-server/src/main/java/com/wiz/demo/sc/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,17 @@
+
+package com.wiz.demo.sc.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
